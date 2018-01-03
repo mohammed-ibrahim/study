1da33ae20c4725c43104750c5ffe687d798647fa
mohammed-ibrahim <mohammed.ibrahim@practo.com>
2018-01-03 15:39:42 +0530

    Adding translation_sequence to right place

--- app.py
+++ app.py
@@ -2,16 +2,44 @@ from flask import Flask, render_template, request, jsonify
 from src.reader import load_from_file, load_json_from_file
 from src.content_manager import get_ruku_content
 
+class Translation:
+    author = None
+    language = ""
+    content_map = {}
+    def __init__(self, author, language, file_name):
+        self.author = author
+        self.language = language
+        self.content_map = load_from_file(file_name)
+
+    def get_ayah(self, ayah_number):
+        return self.content_map[ayah_number]
+
+    def get_ayah_and_details(self, ayah_number):
+        return {
+            "author": self.author,
+            "ayah_translation": self.content_map[str(ayah_number)],
+            "language": self.language
+        }
+
 class AppData:
     arabic = load_from_file("static/arabic/quran-uthmani.txt")
-    translation_yusuf_ali = load_from_file("static/translations/en.yusufali.txt")
-    translation_shakir = load_from_file("static/translations/en.shakir.txt")
-    translation_maulana_maududi = load_from_file("static/translations/ur.maududi.txt")
-    translation_maulana_jalandhry = load_from_file("static/translations/ur.jalandhry.txt")
+
+    translation_yusuf_ali = Translation("Yusuf Ali", "en", "static/translations/en.yusufali.txt")
+    translation_shakir = Translation("Shakir", "en", "static/translations/en.shakir.txt")
+    translation_maulana_maududi = Translation("Maulana Maududi", "ur", "static/translations/ur.maududi.txt")
+    translation_maulana_jalandhry = Translation("Maulana Jalandhry", "ur", "static/translations/ur.jalandhry.txt")
+
     surah_metadata = load_json_from_file("static/metadata/surah_metadata.json")
     ruku_to_surah_mapping = load_json_from_file("static/metadata/ruku_to_surah_mapping.json")
     verse_number_to_root_sequence_mapping = load_json_from_file("static/metadata/verse_number_to_root_sequence_mapping.json")
     root_statistics = load_json_from_file("static/metadata/root_statistics.json")
+
+    translation_sequence = [
+        "translation_maulana_jalandhry",
+        "translation_maulana_maududi",
+        "translation_yusuf_ali",
+        "translation_shakir"
+    ]
 
     def get_surah_and_ruku_metadata(self, ruku_number):
         ruku_number_str = str(ruku_number)
--- src/content_manager.py
+++ src/content_manager.py
@@ -18,7 +18,8 @@ def get_ruku_content(app_data, ruku_number):
         "juz_number": "PENDING",
         "previous_ruku_number": "PENDING",
         "ayah_number_sequence": ruku_data["aayaah_in_ruku"],
-        "aayaah": aayaah
+        "aayaah": aayaah,
+        "translation_sequence": app_data.translation_sequence
     }
 
     return data
@@ -28,8 +29,15 @@ def get_ayah_details(app_data, ayah_number):
         "ayah_number": ayah_number,
         "arabic_content": {
             "uthmani": app_data.arabic[str(ayah_number)]
+        },
+        "translations": {
+
         }
     }
 
+    ayah_details["translations"]["translation_yusuf_ali"] = app_data.translation_yusuf_ali.get_ayah_and_details(ayah_number);
+    ayah_details["translations"]["translation_shakir"] = app_data.translation_shakir.get_ayah_and_details(ayah_number);
+    ayah_details["translations"]["translation_maulana_maududi"] = app_data.translation_maulana_maududi.get_ayah_and_details(ayah_number);
+    ayah_details["translations"]["translation_maulana_jalandhry"] = app_data.translation_maulana_jalandhry.get_ayah_and_details(ayah_number);
 
     return ayah_details
